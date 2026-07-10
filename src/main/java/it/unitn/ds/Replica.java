d2195bf9f25285eacbd429856bb084fa4d3ee9c5
ozneroL541 <l.21radice@gmail.com>
2026-07-10 17:40:11 +0200

    fix: initialize updateClock in Replica class

--- src/main/java/it/unitn/ds/Replica.java
+++ src/main/java/it/unitn/ds/Replica.java
@@ -9,7 +9,6 @@ import java.util.*;
 public class Replica extends AbstractReplica {
     /** List of positions in a replica */
     private final int[] positions = new int[AbstractReplica.POSITIONS_LIST_LENGTH];
-    private final UpdateClock updateClock = new UpdateClock();
     private int coordinator_id = 0;
     /** List of all the replicas of the system. Integer is the id of the replica, ActorRef is the reference of the Replica inside AKKA */
     private Map<Integer, ActorRef> replicas;
